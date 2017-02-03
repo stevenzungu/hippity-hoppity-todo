77feb18d1a03059c2269e2f67e1d514b3b834295
Mac <Mac@Macs-Air.itpro.tv>
2017-02-02 20:49:57 -0500

    Create the javascript literal that is managing the application

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,31 @@
-console.log('running!!!');
+var todoApp = {
+  todos: [], //our data source/store
+  init: function(){
+    this.cacheDom();
+    this.addEventListeners();
+    this.render();
+  },
+  cacheDom: function(){
+    this.createButton = document.querySelector("#create");
+    this.taskInput = document.querySelector("#task");
+    this.list = document.querySelector("#list");
+},
+  render: function(){
+    var listItemsFromTodos = this.todos
+                                 .map(function(todo){
+                                   return "<li>" + todo + "</li>";
+                                 })
+                                 .join("");
+    this.list.innerHTML = listItemsFromTodos;
+  },
+  addEventListeners: function() {
+    this.createButton.addEventListener('click', this.addTodo);
+  },
+  addTodo: function() {
+      var taskValue = todoApp.taskInput.value; //specific to input field
+      todoApp.todos.push(taskValue);
+      todoApp.render();
+  }
+};
+
+todoApp.init();
